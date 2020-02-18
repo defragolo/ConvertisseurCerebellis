1248b17f18e555f96bbaa63787f6203730349282
benjamin DELPECH <benjamin.delpech66@gmail.com>
2020-02-18 15:22:23 +0100

    Corrections

--- Program.cs
+++ Program.cs
@@ -6,33 +6,64 @@ namespace ConvertisseurCerebellis
     {
         static void Main(string[] args)
         {
+            hexaConversion();
+        }
+
+        private static void hexaConversion()
+        {
             while (true)
             {
-
-                Console.WriteLine("Veuillez saisir le code Hexadecimal : " + " \"0\" " + " pour quitter le programme");
-                string hexValues = Console.ReadLine();
-
-                int number;
-                bool success = Int32.TryParse(hexValues, out number);
-                if (success && (Convert.ToInt32(hexValues, 10) == 0))
+                string hexValString = askUserToTypeHexa();
+                bool success = Int32.TryParse(hexValString, out int number);
+                if (success && (Convert.ToInt32(hexValString, 10) == 0))
                 {
                     break;
                 }
-                //string hexValues = "48 65 6C 6C 6F 20 57 6F 72 6C 64 21";
-                string[] hexValuesSplit = hexValues.Split(' ');
-                foreach (string hex in hexValuesSplit)
-                {
-                    // Convert the number expressed in base-16 to an integer.
-                    int value = Convert.ToInt32(hex, 16);
-                    // Get the character corresponding to the integral value.
-                    string stringValue = Char.ConvertFromUtf32(value);
-                    char charValue = (char)value;
-                    //int intValue = (int)value;
-                    Console.WriteLine("hexadecimal value = {0}, int value = {1}, char value = {2} or {3}",
-                                        hex, value, stringValue, charValue);
-                }
-
+                uint numTest = intConversion(hexValString);
+                float floatTest = floatConversion(numTest);
+                string stringTest = stringConversion(hexValString);
+                showConversions(numTest, floatTest, stringTest);
             }
+        }
+
+        private static string askUserToTypeHexa()
+        {
+            Console.WriteLine("Veuillez saisir le code Hexadecimal : " + " \"0\" " + " pour quitter le programme");
+            string hexValString = Console.ReadLine();
+            return hexValString;
+        }
+
+        private static void showConversions(uint numTest, float floatTest, string stringTest)
+        {
+            Console.WriteLine("String val = {0}", stringTest);
+            Console.WriteLine("Int val = {0}", numTest);
+            Console.WriteLine("Float val = {0}", floatTest);
+        }
+
+        private static string stringConversion(string hexValString)
+        {
+            //string conversion
+            byte[] raw = new byte[hexValString.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hexValString.Substring(i * 2, 2), 16);
+            }
+            string stringTest = System.Text.Encoding.UTF8.GetString(raw);
+            return stringTest;
+        }
+
+        private static float floatConversion(uint numTest)
+        {
+            //float conversion
+            byte[] floatVals = BitConverter.GetBytes(numTest);
+            float floatTest = BitConverter.ToSingle(floatVals, 0);
+            return floatTest;
+        }
+
+        private static uint intConversion(string hexValString)
+        {
+            // int conversion
+            return uint.Parse(hexValString, System.Globalization.NumberStyles.AllowHexSpecifier);
         }
     }
 }
